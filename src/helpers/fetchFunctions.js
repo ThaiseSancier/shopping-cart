750726d9dd4aa8c9ddbdefbd2a264e37854ce80d
Thaise Sancier <lpthaise@gmail.com>
2023-03-31 13:50:00 -0300

    Requisito 2 - Debug da mensagem de erro

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,14 +3,14 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (query) => {
+  if (!query) {
+    throw new Error('Termo de busca não informado');
+  }
   try {
     const fetchResponse = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
     const returnData = await fetchResponse.json();
     return returnData.results;
   } catch (error) {
-    if (!query) {
-      throw new Error('Termo de busca não informado');
-    }
     return error.message;
   }
 };
