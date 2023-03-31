7d125405cc6e91750046b872e6f3cb241cc93d4b
Thaise Sancier <lpthaise@gmail.com>
2023-03-31 13:13:37 -0300

    Requisito 3 - Listagem de produtos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsList = await fetchProductsList('computador');
+const classProducts = document.querySelector('.products');
+productsList.forEach((products) => {
+  const productsSection = createProductElement(products);
+  classProducts.appendChild(productsSection);
+});
