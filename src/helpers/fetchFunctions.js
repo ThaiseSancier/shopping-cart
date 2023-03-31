6de0a6c3d90d1fa917b8e7822db8d32ca964fc19
Thaise Sancier <lpthaise@gmail.com>
2023-03-31 12:27:58 -0300

    Requisito 2 - Implementação da função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,15 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  try {
+    const fetchResponse = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const returnData = await fetchResponse.json();
+    return returnData.results;
+  } catch (error) {
+    if (!query) {
+      throw new Error('Termo de busca não informado');
+    }
+    return error.message;
+  }
 };
